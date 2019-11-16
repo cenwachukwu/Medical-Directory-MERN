361b89245f6e56c6e7d97ade428df97a5feb4518
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-11-16 13:58:43 -0500

    updated doctors

--- mern-react-app/src/doctors.js
+++ mern-react-app/src/doctors.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Doctors extends Component{
-    render(){
+class Doctors extends Component {
+    render() {
         return (
             <div>
                 <h1>Meet your medical practioners</h1>
                 <h2>Find your Doctors</h2>
-                <button>
-                    <Link to="/Doctors/searchbyprofile">Search by names</Link>
-                </button>
-                <button>
-                    <Link to="/Doctors/searchbyinsurance">Search by insurance</Link>
-                </button>
-                <button>
-                    <Link to="/Doctors/searchbyspeciality">Search by speciality</Link>
-                </button>
+                <Link to="/Doctors/searchbyprofile">
+                    <button>
+                        Search by names
+                    </button>
+                </Link>
+                <Link to="/Doctors/searchbyinsurance">
+                    <button>
+                        Search by insurance
+                    </button>
+                </Link>
+                <Link to="/Doctors/searchbyspeciality">
+                    <button>
+                        Search by speciality
+                    </button>
+                </Link>
             </div>
         )
     }
